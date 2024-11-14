3cd9161141e13fcd15a975253a36f02383a75c00
djpark0402 <djpark0402@raoncorp.com>
2024-11-14 17:28:41 +0900

    build: modify git subtree script

---
+++ add_script.sh
@@ -0,0 +1,2 @@
+git subtree add --prefix=did-ca-ios https://github.com/djpark0402/did-ca-ios develop
+git subtree add --prefix=did-client-sdk-ios https://github.com/djpark0402/did-client-sdk-ios develop
--- pull_script.sh
+++ pull_script.sh
@@ -1,4 +1,2 @@
-git subtree pull --prefix=subtree-child1 https://github.com/djpark0402/subtree-child1 main
-git subtree pull --prefix=subtree-child2 https://github.com/djpark0402/subtree-child2 main
-git subtree pull --prefix=subtree-child3 https://github.com/djpark0402/subtree-child3 main
-
+git subtree pull --prefix=did-ca-ios https://github.com/djpark0402/did-ca-ios develop
+git subtree pull --prefix=did-client-sdk-ios https://github.com/djpark0402/did-client-sdk-ios develop
